6f1c389593f7ae0f07b439d1460859894e5bdfe1
Darius Kaminskas <dariuskaminskas@outlook.com>
2023-07-06 18:52:26 +0300

    some styling

--- views/nav.php
+++ views/nav.php
@@ -1,34 +1,31 @@
 <nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
-    <a class="navbar-brand" href="./">OOP Bankas</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+    <img class = "logo" src="./img/bank.png" alt="logo">
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="<?= URL ?>">Pagrindinis</a>
+          <a class="nav-link onHover" aria-current="page" href="<?= URL ?>">Pagrindinis</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="<?= URL .'/accounts'?>">Sąskaitos</a>
+          <a class="nav-link onHover" aria-current="page" href="<?= URL .'/accounts'?>">Sąskaitos</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="<?= URL .'/accounts/create'?>">Pridėti sąskaitą</a>
+          <a class="nav-link onHover" href="<?= URL .'/accounts/create'?>">Pridėti sąskaitą</a>
         </li>
       </ul>
     </div>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-        <?php if (isset($_SESSION['email'])) : ?>
-        <form action="<?= URL . '/logout' ?>" method="post" style="display: inline;">
-            <button style="background: none; border: none; color: white " type="submit" class="w3-bar-item w3-button">Atsijungti</button>
-        </form>
-        <?php else: ?>
-            <a href="<?= URL . '/login' ?>" class="w3-bar-item w3-button">Prisijungti</a>
-        <?php endif ?>
-        </li>
-      </ul>
-    </div>
+  </div>
+  <div class="collapse navbar-collapse" id="navbarNav" style = "margin-right: 8px">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+      <?php if (isset($_SESSION['email'])) : ?>
+      <form action="<?= URL . '/logout' ?>" method="post" style="display: inline;">
+          <button type="submit" class="w3-bar-item w3-button loginbutton onHover">Atsijungti</button>
+      </form>
+      <?php else: ?>
+          <a href="<?= URL . '/login' ?>" class="w3-bar-item w3-button loginbutton onHover">Prisijungti</a>
+      <?php endif ?>
+      </li>
+    </ul>
   </div>
 </nav>
